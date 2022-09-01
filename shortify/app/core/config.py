bcdbc74e08ccc394b1fb2711be0211a4bb9a4979
Iliya <IHosseini083@gmail.com>
2022-09-01 17:43:19 +0430

    Release v0.0.3

--- shortify/app/core/config.py
+++ shortify/app/core/config.py
@@ -8,7 +8,7 @@ from pydantic.networks import EmailStr
 class Settings(BaseSettings):
     # Application
     PROJECT_NAME: str = "Shortify"
-    PROJECT_VERSION: str = "0.0.2"
+    PROJECT_VERSION: str = "0.0.3"
     API_V1_STR: str = "v1"
     DEBUG: bool = True
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
