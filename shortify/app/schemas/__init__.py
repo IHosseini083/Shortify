54cc7bce5c66e43fb656372ad0fbfbef9f0f8b74
Iliya <IHosseini083@gmail.com>
2022-09-06 01:22:03 +0430

    Create schema for sorting operations.

--- shortify/app/schemas/__init__.py
+++ shortify/app/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .pagination import Paginated, PaginationParams
+from .sorting import SortingParams
 from .token import AuthToken, AuthTokenPayload
 from .url import ShortUrl, ShortUrlCreate
 from .user import User, UserCreate, UserInDB, UserUpdate
