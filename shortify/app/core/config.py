638c3a0069484ab52f5fa28061e3172a2752477a
Iliya <IHosseini083@gmail.com>
2022-10-20 20:41:59 +0330

    Refactor logging functionality

--- shortify/app/core/config.py
+++ shortify/app/core/config.py
@@ -1,5 +1,6 @@
 import logging
 import secrets
+from pathlib import Path
 from typing import List, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, EmailStr, MongoDsn, validator
@@ -12,13 +13,23 @@ class Settings(BaseSettings):
     API_V1_STR: str = "v1"
     DEBUG: bool = True
     BACKEND_CORS_ORIGINS: Union[str, List[AnyHttpUrl]] = []
+
+    # Logging
     LOG_LEVEL: str = "INFO"
+    LOG_FILE_PATH: str = "logs/shortify.log"
 
     @validator("LOG_LEVEL")
     def log_level_validator(cls, v: str) -> str:  # noqa
         v = v.upper()
         if not hasattr(logging, v):
             raise ValueError(f"Invalid log level: {v!r}")
+        return v
+
+    @validator("LOG_FILE_PATH")
+    def check_log_file_path(cls, v: str) -> str:  # noqa
+        if not v.endswith(".log"):
+            raise ValueError(f"Invalid log file path: {v!r} (must end with .log)")
+        Path(v).parent.mkdir(parents=True, exist_ok=True)
         return v
 
     # Custom validators that have 'pre' set to 'True', will be called before
--- shortify/app/core/logging.py
+++ shortify/app/core/logging.py
@@ -7,20 +7,28 @@ import uvicorn
 
 from shortify.app.core.config import settings
 
+LOG_LEVEL = "DEBUG" if settings.DEBUG else settings.LOG_LEVEL
+
+EventDict = structlog.types.EventDict
+
+
+def remove_color_message(_, __, event_dict: EventDict) -> EventDict:
+    event_dict.pop("color_message", None)
+    return event_dict
+
+
 # Processors that have nothing to do with output,
 # e.g. add timestamps or log level names.
 SHARED_PROCESSORS: Tuple[structlog.types.Processor, ...] = (
     structlog.contextvars.merge_contextvars,
-    structlog.stdlib.add_logger_name,
     structlog.stdlib.add_log_level,
-    # Add a timestamp in ISO 8601 format.
-    structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
     # Add extra attributes of LogRecord objects to the event dictionary
     # so that values passed in the extra parameter of log methods pass
     # through to log output.
     structlog.stdlib.ExtraAdder(),
+    # Add a timestamp in ISO 8601 format.
+    structlog.processors.TimeStamper(fmt="ISO"),
 )
-
 LOGGING_CONFIG: Dict[str, Any] = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -28,21 +36,31 @@ LOGGING_CONFIG: Dict[str, Any] = {
         "json": {
             "()": structlog.stdlib.ProcessorFormatter,
             # Render the final event dict as JSON.
-            "processor": structlog.processors.JSONRenderer(indent=4, sort_keys=True),
+            "processor": structlog.processors.JSONRenderer(),
             "foreign_pre_chain": SHARED_PROCESSORS,
         },
-        "console": {
+        "colored": {
             "()": structlog.stdlib.ProcessorFormatter,
-            "processor": structlog.dev.ConsoleRenderer(),
+            "processors": [
+                remove_color_message,
+                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                structlog.dev.ConsoleRenderer(colors=True),
+            ],
             "foreign_pre_chain": SHARED_PROCESSORS,
         },
         **uvicorn.config.LOGGING_CONFIG["formatters"],
     },
     "handlers": {
         "default": {
-            "level": settings.LOG_LEVEL,
+            "level": LOG_LEVEL,
             "class": "logging.StreamHandler",
-            "formatter": "json" if not settings.DEBUG else "console",
+            "formatter": "colored",
+        },
+        "file": {
+            "level": LOG_LEVEL,
+            "class": "logging.FileHandler",
+            "filename": settings.LOG_FILE_PATH,
+            "formatter": "json",
         },
         "uvicorn.access": {
             "level": "INFO",
@@ -57,7 +75,7 @@ LOGGING_CONFIG: Dict[str, Any] = {
     },
     "loggers": {
         "": {
-            "handlers": ["default"],
+            "handlers": ["default", "file"],
             "level": "INFO",
             "propagate": True,
         },
@@ -76,6 +94,7 @@ LOGGING_CONFIG: Dict[str, Any] = {
 
 
 def setup_logging() -> None:
+    logging.config.dictConfig(LOGGING_CONFIG)
     # noinspection PyTypeChecker
     structlog.configure(
         processors=[
@@ -91,13 +110,6 @@ def setup_logging() -> None:
             structlog.processors.format_exc_info,
             # If some value is in bytes, decode it to a unicode str.
             structlog.processors.UnicodeDecoder(),
-            structlog.processors.CallsiteParameterAdder(
-                {
-                    structlog.processors.CallsiteParameter.FILENAME,
-                    structlog.processors.CallsiteParameter.FUNC_NAME,
-                    structlog.processors.CallsiteParameter.LINENO,
-                }
-            ),
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         context_class=dict,
@@ -105,4 +117,3 @@ def setup_logging() -> None:
         wrapper_class=structlog.stdlib.AsyncBoundLogger,
         cache_logger_on_first_use=True,
     )
-    logging.config.dictConfig(LOGGING_CONFIG)
